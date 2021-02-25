f95afbdeb5af97cb38685ce40e2af28f29897417
TonyJem <a.jemanov@gmail.com>
2021-02-25 02:52:49 +0200

    Disable "Submit: button in "Login" screen if any shown textFields are empty

--- BreakingBadFan App/BreakingBadFan App/ViewControllers/LoginViewController.swift
+++ BreakingBadFan App/BreakingBadFan App/ViewControllers/LoginViewController.swift
@@ -9,11 +9,20 @@ class LoginViewController: MainViewController {
     @IBOutlet private weak var confirmPasswordTextField: UITextField!
     @IBOutlet private weak var submitButton: UIButton!
     
+    private var loginFlowTexfields: [UITextField] = []
+    private var registerFlowTexfields: [UITextField] = []
+    private var shownTextfields: [UITextField] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupUI()
 
+        setupUI()
+        prepareTextFields()
+    }
+    
+    @objc func textChanged(_ textField: UITextField) {
+        shownTextfields = confirmPasswordTextField.isHidden ? loginFlowTexfields : registerFlowTexfields
+        submitButton.isEnabled = !shownTextfields.contains { $0.text!.isEmpty }
     }
     
     @IBAction private func loginSegmentedControlChanged(_ sender: UISegmentedControl) {
@@ -33,6 +42,15 @@ private extension LoginViewController {
         view.backgroundColor = Colors.mainBackgroundColor
         loginSegmentedControl.selectedSegmentIndex = 0
         confirmPasswordTextField.isHidden = true
+        submitButton.isEnabled = false
+    }
+    
+    private func prepareTextFields() {
+        usernameTextField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
+        confirmPasswordTextField.addTarget(self, action: #selector(textChanged(_:)), for: .editingChanged)
+        loginFlowTexfields = [usernameTextField, passwordTextField]
+        registerFlowTexfields = [usernameTextField, passwordTextField, confirmPasswordTextField]
     }
     
 }
