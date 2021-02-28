92108010df230fbf75ab2b6d6da0ac920b263204
TonyJem <a.jemanov@gmail.com>
2021-02-28 02:24:26 +0200

    Create "EpisodesViewController" and connect to storyboard
    extension EpisodesViewController:

--- BreakingBadFan App/BreakingBadFan App/ViewControllers/EpisodesViewController.swift
+++ BreakingBadFan App/BreakingBadFan App/ViewControllers/EpisodesViewController.swift
@@ -2,7 +2,10 @@ import UIKit
 
 class EpisodesViewController: UIViewController {
 
+    @IBOutlet private var seasonsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
 }
