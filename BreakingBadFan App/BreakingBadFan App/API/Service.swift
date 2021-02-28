3d907fd6e1c015390f21e1cb333cc72dcd5a72bd
TonyJem <a.jemanov@gmail.com>
2021-02-28 23:14:28 +0200

    Minor refactor

--- BreakingBadFan App/BreakingBadFan App/API/Service.swift
+++ BreakingBadFan App/BreakingBadFan App/API/Service.swift
@@ -26,28 +26,32 @@ class BreakingBadService {
     static func getCharacters(parameters: String, characterId: String?, completion: @escaping ([Character]) -> Void) {
         
         let url: String = "\(BreakingBadApiConstants.baseUrl)\(parameters)\(characterId ?? "")"
-        
         print("[Started] getCharacters() url == ", url)
-        
         URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
-            
-            guard let data = data, error == nil else {
-                return
-            }
+            guard let data = data, error == nil else { return }
             
             do {
                 let response: [Character] = try JSONDecoder().decode([Character].self, from: data)
                 completion(response)
-                
                 print("[Success] getCharacters() [Character].count == ", response.count)
                 
             } catch {
-                
                 print("[Error] getCharacters() ==", error.localizedDescription)
-                
             }
-            
         }).resume()
     }
     
 }
+
+
+/*
+
+BreakingBadService.getCharacters(parameters: BreakingBadApiConstants.paramCharacters, characterId: nil, completion: { response in
+    print("🟡 [Character].count == ", response.count)
+})
+
+BreakingBadService.getCharacters(parameters: BreakingBadApiConstants.paramCharacters, characterId: "9", completion: { response in
+    print("🟡 Selected Character nickname \(response[0].nickname)")
+})
+ 
+ */
